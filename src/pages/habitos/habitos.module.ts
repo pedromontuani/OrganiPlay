0ca45d84bf0af0f9365235f6ad925b0ba02de01e
Matheus Jose <36504902+MatheusDatena@users.noreply.github.com>
2018-05-11 10:39:50 -0300

    Add files via upload

---
+++ src/pages/habitos/habitos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HabitosPage } from './habitos';
+
+@NgModule({
+  declarations: [
+    HabitosPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(HabitosPage),
+  ],
+})
+export class HabitosPageModule {}
